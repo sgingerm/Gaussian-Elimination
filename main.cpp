8363318574d686898c105848c4b090e37287d936
mytest <dsardrwarf>
2022-12-26 22:05:23 +0800

    统一键盘输入和文件键输入的格式

--- main.cpp
+++ main.cpp
@@ -14,8 +14,8 @@ int m, n;
 void print() {
   cout<<""<<endl;
   cout<<"----------"<<endl;
-  for (int i = 1; i <= m; i++) {
-    for (int j = 1; j <= n; j++){
+  for (int i = 0; i < m; i++) {
+    for (int j = 0; j < n; j++){
       cout<< fixed << setprecision(4) <<a[i][j]<<"  ";
     }
     cout<<""<<endl;
@@ -23,22 +23,22 @@ void print() {
 }
 void work() {
   print();
-  for (int j = 1; j <= n - 1; j++) { //从第一列到第n-1列
-    for (int i = j + 1; i <= m; i++) {   //从第j+1行到第m行
+  for (int j = 0; j < n - 1; j++) { //从第一列到第n-1列
+    for (int i = j + 1; i < m; i++) {   //从第j+1行到第m行
       if (a[j][i] == 0) continue;
       double t = - a[i][j] / a[j][j];
-      for (int k = j; k <= n; k++) a[i][k] += a[j][k] * t;
+      for (int k = j; k < n; k++) a[i][k] += a[j][k] * t;
     }
     print();
   }
   cout<<"----------已化为阶梯型矩阵----------"<<endl;
   //判断是否有解
-  for (int i = 1; i <= m; i++) {
+  for (int i = 0; i < m; i++) {
     int flag = 0;
-    for (int j = 1; j <= n - 1; j++) {
+    for (int j = 0; j < n - 1; j++) {
       if (a[i][j] != 0) flag = 1;
     }
-    if (flag == 1 && a[i][n] == 0) {
+    if (flag == 1 && a[i][n-1] == 0) {
       cout<<"方程组无解"<<endl;
       return;
     }
@@ -48,7 +48,7 @@ void work() {
     cout<<"方程组有无穷多组解\n"<<endl;
     return;
   }
-  else for (int i = 1; i <= m; i++) {
+  else for (int i = 0; i < m; i++) {
     if (a[i][i] == 0) {
       cout<<"方程组有无穷多组解\n"<<endl;
       return;
@@ -56,14 +56,14 @@ void work() {
   }
   //求解方程
   cout<<"方程组有唯一解：\n"<<endl;
-  for (int i = m; i >= 1; i--) {
-    x[i] = a[i][n];
-    for (int j = i + 1; j <= n - 1; j++) x[i] -= a[i][j] * x[j];
+  for (int i = m - 1; i >= 0; i--) {
+    x[i] = a[i][n-1];
+    for (int j = i + 1; j < n - 1; j++) x[i] -= a[i][j] * x[j];
     x[i] /= a[i][i];
   }
-  for (int i = 1; i <= n - 1; i++) 
+  for (int i = 0; i < n - 1; i++) 
   {
-    cout<< i << "x[" << i << "]=" << x[i] <<endl;
+    cout<< "x[" << i << "]=" << x[i] <<endl;
   }
 }
 
@@ -123,26 +123,36 @@ void fileRead(double (&matrix)[3][3],int m,int n,string fileName=".\\data.txt"){
   inFile.close();
 }
 
-
 int main() {
-  double a[3][3];
-  fileRead(a,1,1);
+  double matrix[3][3];
+  string line;
+  cout<<"请输入增广矩阵的行数：\n"<<endl;
+  cin>>m;
+  cin.ignore(std::numeric_limits< streamsize >::max(), '\n');
+  cout<<"请输入增广矩阵的列数：\n"<<endl;
+  cin>>n;
+  cin.ignore(std::numeric_limits< streamsize >::max(), '\n');
+  cout<<"请输入整个增广矩阵：\n"<<endl;
+  for (int i = 0; i < m; i++)
+  {
+    getline(cin,line);
+    vector<string>  row =  split(line," ");
+    for ( int j = 0; j < n; j++ )
+    { 
+	    a[i][j] = stringToDouble(row[j]); 
+    }
+  }
+
+  work();
+
+  /*
+  fileRead(matrix,1,1);
   for(int i = 0;i<3;i++){
     for(int j = 0; j < 3; j++){
-      cout << a[i][j] << " ";
+      cout << matrix[i][j] << " ";
     }
     cout<<endl;
   }
-  /*
-  cout<<"请输入增广矩阵的行数：\n"<<endl;
-  cin>>m;
-  cout<<"请输入增广矩阵的列数：\n"<<endl;
-  cin>>n;
-  cout<<"请输入整个增广矩阵：\n"<<endl;
-  for (int i = 1; i <= m; i++)
-    for (int j = 1; j <= n; j++)
-      cin>>a[i][j];
-  work();
   */
   return 0;
 }
