26cd1cdc529fde388e2d547099b43e594f7daef7
mytest <dsardrwarf>
2022-12-26 16:06:58 +0800

    微调

--- main.cpp
+++ main.cpp
@@ -1,16 +1,24 @@
-//初始化程序
 #include <stdio.h>
+#include<iostream>
+#include<iomanip>
+#include<cmath>
+#include<string>
+#include<algorithm>
+#include<bits/stdc++.h>
+using namespace std;
 double a[21][21], x[21];
 int m, n;
 
 void print() {
-  printf("\n------->\n");
+  cout<<""<<endl;
+  cout<<"----------"<<endl;
   for (int i = 1; i <= m; i++) {
-    for (int j = 1; j <= n; j++) printf("%.2lf ", a[i][j]);
-    printf("\n");
+    for (int j = 1; j <= n; j++){
+      cout<< fixed << setprecision(4) <<a[i][j]<<"  ";
+    }
+    cout<<""<<endl;
   }
 }
-
 void work() {
   print();
   for (int j = 1; j <= n - 1; j++) { //从第一列到第n-1列
@@ -21,7 +29,7 @@ void work() {
     }
     print();
   }
-  printf("\n----------已化为阶梯型矩阵----------\n\n");
+  cout<<"----------已化为阶梯型矩阵----------"<<endl;
   //判断是否有解
   for (int i = 1; i <= m; i++) {
     int flag = 0;
@@ -29,39 +37,42 @@ void work() {
       if (a[i][j] != 0) flag = 1;
     }
     if (flag == 1 && a[i][n] == 0) {
-      printf("方程组无解\n");
+      cout<<"方程组无解"<<endl;
       return;
     }
   }
   //判断是否有无穷多解
   if (m < n - 1) {
-    printf("方程组有无穷多组解\n");
+    cout<<"方程组有无穷多组解\n"<<endl;
     return;
   }
   else for (int i = 1; i <= m; i++) {
     if (a[i][i] == 0) {
-      printf("方程组有无穷多组解\n");
+      cout<<"方程组有无穷多组解\n"<<endl;
       return;
     }
   }
   //求解方程
-  printf("方程组有唯一解：\n");
+  cout<<"方程组有唯一解：\n"<<endl;
   for (int i = m; i >= 1; i--) {
     x[i] = a[i][n];
     for (int j = i + 1; j <= n - 1; j++) x[i] -= a[i][j] * x[j];
     x[i] /= a[i][i];
   }
-  for (int i = 1; i <= n - 1; i++) printf("x%d = %.2lf\n", i, x[i]);
+  for (int i = 1; i <= n - 1; i++) 
+  {
+    cout<< i << "x[" <<i<<"]="<<x[i]<<endl;
+  }
 }
-
-
 int main() {
-  printf("请输入增广矩阵的行数和列数：\n");
-  scanf("%d%d", &m, &n);
-  printf("请输入整个增广矩阵：\n");
+  cout<<"请输入增广矩阵的行数：\n"<<endl;
+  cin>>m;
+  cout<<"请输入增广矩阵的列数：\n"<<endl;
+  cin>>n;
+  cout<<"请输入整个增广矩阵：\n"<<endl;
   for (int i = 1; i <= m; i++)
     for (int j = 1; j <= n; j++)
-      scanf("%lf", &a[i][j]);
+      cin>>a[i][j];
   work();
   return 0;
 }
